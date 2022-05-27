c949721e15d1f0160aacad0fb22f049eb2224ecc
Marcos Paulo <dev.marcospereira@gmail.com>
2022-05-27 19:42:05 -0300

    requisito 12

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 //SELEÇÃO DO HTML.
 let selected = document.getElementsByClassName('selected')[0].classList[1];
+let colorStore = 'background-color: rgb(0, 0, 0)';
 
-const body = document.querySelectorAll('body');
 const title = document.getElementById('title');
 
 const colorPalette = document.getElementById('color-palette');
+let colorRed = document.getElementsByClassName('color-red');
+let colorBlue = document.getElementsByClassName('color-blue');
+let colorGreen = document.getElementsByClassName('color-green'); 
 let color = document.getElementsByClassName('color');
 
 const clearBoard = document.getElementById('clear-board');
@@ -21,6 +24,35 @@ const board4 = document.getElementsByClassName('board4');
 const board5 = document.getElementsByClassName('board5');
 let pixel = document.getElementsByClassName('pixel');
 
+//INICIA COM BACKGROUND-COLOR: BLACK;
+function colorize(paint) {
+    let aux = paint.classList[1];
+    paint.classList.replace(aux, selected);
+  }
+  
+  pixelBoard.addEventListener('click', function (event) {
+    colorize(event.target);
+    // console.log(event.target); //TESTAR SELEÇÃO
+  })
+
+//GERA CORES ALETORIAS NA PALETA DE CORES.
+function generateColor() {
+
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  
+  for (let e = 0; e < 6; e += 1) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  
+  return color;
+  
+}
+
+for (let f = 1; f < color.length; f += 1){
+  let capatAux = 'background-color: ' + generateColor();
+  color[f].setAttribute('style', capatAux);
+}
 
 
 //SELECIONA COR NA PALETA.
@@ -35,19 +67,26 @@ function colorSelect(selection) {
 }
 
 colorPalette.addEventListener('click', function (event) {
+  let teste = window.getComputedStyle(event.target);
+  let teste2 = teste.getPropertyValue('background-color');
+  colorStore = teste2;
   colorSelect(event.target);
+  console.log(colorStore);
   // console.log(color); //TESTAR SELEÇÃO
   // console.log(selected); //TESTAR SELEÇÃO
 })
 
 //COLORE PIXEL.
-function colorize(paint) {
-  let aux = paint.classList[1];
-  paint.classList.replace(aux, selected);
+function colorize2(paint) {
+  // let aux = window.getComputedStyle(paint);
+  // let aux2 = aux.getPropertyValue('background-color');
+  // paint.setAttribute('style', 'background-color' + aux2)
+  // let aux = paint.classList[1];
+  paint.setAttribute('style', 'background-color: ' + colorStore);
 }
 
 pixelBoard.addEventListener('click', function (event) {
-  colorize(event.target);
+  colorize2(event.target);
   // console.log(event.target); //TESTAR SELEÇÃO
 })
 
