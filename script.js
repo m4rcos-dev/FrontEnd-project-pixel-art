7345a2693a6ac771767590e82ed831aeee321627
Marcos Paulo <dev.marcospereira@gmail.com>
2022-05-27 16:04:03 -0300

    requisito 11

--- script.js
+++ script.js
@@ -58,19 +58,25 @@ clearBoard.addEventListener('click', function () {
 
 //MUNDA QUANTIDADE DE PIXEL PARA PINTAR.
 function sizeBoard() {
-
-  for (let c = 0; c < imputBoardSize.value; c += 1) {
-    let newLine = document.createElement('div');
-    newLine.id = 'board' + c;
-    newLine.className = 'newLine';
-    pixelBoard.appendChild(newLine);
-    for (let d = 0; d < imputBoardSize.value; d += 1) {
-      let newColun = document.createElement('div');
-      newColun.className = 'pixel color-white size border horizontal';
-      let aux = document.getElementById('board' + c);
-      aux.appendChild(newColun);
+  let imputValue = imputBoardSize.value;
+  if (imputValue < 5) {
+    imputValue = 5;
+  } else if (imputValue > 50) {
+    imputValue = 50;
+  } 
+    for (let c = 0; c < imputValue; c += 1) {
+      let newLine = document.createElement('div');
+      newLine.id = 'board' + c;
+      newLine.className = 'newLine';
+      pixelBoard.appendChild(newLine);
+      for (let d = 0; d < imputValue; d += 1) {
+        let newColun = document.createElement('div');
+        newColun.className = 'pixel color-white size border horizontal';
+        let aux = document.getElementById('board' + c);
+        aux.appendChild(newColun);
+      }
     }
-  }
+  
 }
 // console.log(pixelBoard); //TESTAR TAMANHO.
 
